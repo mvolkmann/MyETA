cef37f8a788a5ce5872e5690b4dd061bf090cb06
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-03-02 15:35:56 -0600

    initial commit

--- MyETA/ContentView.swift
+++ MyETA/ContentView.swift
@@ -1,19 +1,41 @@
-//
-//  ContentView.swift
-//  MyETA
-//
-//  Created by Mark Volkmann on 3/2/23.
-//
-
+import MessageUI
 import SwiftUI
 
+private class MessageComposerDelegate: NSObject,
+    MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(
+        _ controller: MFMessageComposeViewController,
+        didFinishWith result: MessageComposeResult
+    ) {
+        controller.dismiss(animated: true)
+    }
+}
+
 struct ContentView: View {
+    private let messageComposeDelegate = MessageComposerDelegate()
+
+    private func presentMessageCompose() {
+        guard MFMessageComposeViewController.canSendText() else { return }
+
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        if let vc = windowScene?.windows.first?.rootViewController {
+            let composeVC = MFMessageComposeViewController()
+            composeVC.messageComposeDelegate = messageComposeDelegate
+            composeVC.subject = "My ETA" // used?
+            composeVC.recipients = ["314-398-6256"]
+            composeVC.body = "I will arrive close to 5:30 PM."
+            vc.present(composeVC, animated: true)
+        }
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Send") {
+                presentMessageCompose()
+            }
+            .buttonStyle(.borderedProminent)
+            Spacer()
         }
         .padding()
     }
--- MyETA/MyETAApp.swift
+++ MyETA/MyETAApp.swift
@@ -1,10 +1,4 @@
-//
-//  MyETAApp.swift
-//  MyETA
-//
-//  Created by Mark Volkmann on 3/2/23.
-//
-
+import MessageUI
 import SwiftUI
 
 @main
