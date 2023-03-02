d1af229f70045724dd2643f99c49ef07f55cad23
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-03-02 10:03:25 -0600

    Initial Commit

---
+++ MyETA/MyETAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyETAApp.swift
+//  MyETA
+//
+//  Created by Mark Volkmann on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MyETAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
