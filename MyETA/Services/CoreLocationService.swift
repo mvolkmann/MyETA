f39751a99276ec748a5e1ddd8cafb4e604431aa9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-03-03 13:11:26 -0600

    progress

--- MyETA/Services/CoreLocationService.swift
+++ MyETA/Services/CoreLocationService.swift
@@ -3,40 +3,28 @@ import SwiftUI
 
 struct CoreLocationService {
     static func getPlacemark(
-        from location2D: CLLocationCoordinate2D
+        from location: CLLocation
     ) async throws -> CLPlacemark? {
-        let location = CLLocation(
-            latitude: location2D.latitude,
-            longitude: location2D.longitude
-        )
-        return try await Self.getPlacemark(from: location)
-    }
-
-    static func getPlacemark(from location: CLLocation) async throws
-        -> CLPlacemark? {
         // Cannot call this more than 50 times per second.
         let placemarks = try await CLGeocoder()
             .reverseGeocodeLocation(location)
         if let placemark = placemarks.first {
             return placemark
         } else {
-            throw "no placemarks found"
+            throw "no placemarks found for \(location)"
         }
     }
 
-    static func getPlacemark(from addressString: String) async throws
-        -> CLPlacemark {
-        print(
-            "CoreLocationService.getPlacemark: addressString =",
-            addressString
-        )
+    static func getPlacemark(
+        from addressString: String
+    ) async throws -> CLPlacemark {
         // Cannot call this more than 50 times per second.
         let placemarks = try await CLGeocoder()
             .geocodeAddressString(addressString)
         if let placemark = placemarks.first {
             return placemark
         } else {
-            throw "no placemarks found"
+            throw "no placemarks found for \(addressString)"
         }
     }
 }
